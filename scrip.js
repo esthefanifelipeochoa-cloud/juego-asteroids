4e64c0d352c88a40fb1d2ca1e1505997e0d42c43
esthefanifelipeochoa-cloud <esthefanifelipeochoa@gmail.com>
2026-04-07 23:19:54 -0400

    Add files via upload
    Segundo avance:  se agrego el metodo para el disparo de bombas.

--- scrip.js
+++ scrip.js
@@ -1,30 +1,71 @@
 const canvas=document.getElementById("lienzo");
-canvas.width=500;
-canvas.height=500;
+canvas.width=700;
+canvas.height=700;
 const ctx= canvas.getContext("2d");
-let x=200;
-let y=200;
+let x=250;
+let y=250;
 let angulo=0;
-function dibujar(){
+let pBomX;
+let pBomy;
+let bombas=[];
+function dibujarNave(){
    ctx.clearRect(0,0,canvas.width,canvas.height);
    ctx.save();
    ctx.translate(x,y);
    ctx.rotate(angulo);
+   ctx.strokeStyle="white"
+   ctx.fillStyle="black";
    ctx.beginPath();
    ctx.moveTo(20,0);
    ctx.lineTo(-15,-10);
    ctx.lineTo(-15,10);
    ctx.closePath();
+   ctx.fill();
    ctx.stroke();
    ctx.restore();
-  
+   dibujarBombas();
 }
+
 function girarIzq(){
-    angulo+=0.1;
-    dibujar();
+    angulo-=0.1;
+    dibujarNave();
 }
 function girarDer(){
-    angulo-=0.1;
-    dibujar();
+    angulo+=0.1;
+    dibujarNave();
 }
-dibujar();
+function avanzar(){
+    x=x+10*Math.cos(angulo);
+    y=y+10*Math.sin(angulo);
+    dibujarNave();
+}
+function dibujarBombas(){
+    bombas.forEach((bomba,pos)=>{
+        bomba.x +=bomba.pBomX;
+        bomba.y +=bomba.pBomy;
+        ctx.beginPath();
+        ctx.arc(bomba.x,bomba.y,3,0,Math.PI*2);
+        ctx.fillStyle="white";
+        ctx.fill();
+    });
+   
+}
+function disparar(){
+    let velocidad=5;
+    let bomba={
+        x: x,
+        y: y,
+        pBomX: Math.cos(angulo)*velocidad,
+        pBomy: Math.sin(angulo)*velocidad
+    };
+    bombas.push(bomba);
+   
+}
+function loop(){
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+    dibujarNave();
+    dibujarBombas();
+    requestAnimationFrame(loop);
+}
+
+loop();
